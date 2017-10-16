05e894ee1ae5e2a282faef89adb8e4e2d7840dd8
jnm2 <me@jnm2.com>
2017-10-15 22:31:31 -0400

    Added failing test on ending greediness

---
+++ src/SourceHeaderAnalyzer.Tests/TemplateMatchTests.cs
@@ -0,0 +1,20 @@
+using System.Collections.Immutable;
+using NUnit.Framework;
+using SourceHeaderAnalyzer.Templating;
+
+namespace SourceHeaderAnalyzer.Tests
+{
+    public static class TemplateMatchTests
+    {
+        [Test]
+        public static void Last_segment_is_not_greedy()
+        {
+            var template = new HeaderTemplate(ImmutableArray.Create<TemplateSegment>(
+                new TextTemplateSegment("a ")));
+
+            Assert.That(template.TryMatch("a  ", default, out var result));
+
+            Assert.That(result, Has.Property("Length").EqualTo(2));
+        }
+    }
+}
